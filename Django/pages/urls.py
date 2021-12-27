a0677e6ae01b302b3cca7997c4c6e626fd0ca831
Christine Roethel <roethel.christine@gmail.com>
2021-12-27 14:46:30 -0500

    added tracker page

--- Django/pages/urls.py
+++ Django/pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, TrackerPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('tracker/', TrackerPageView.as_view(), name='tracker'),
 ]
